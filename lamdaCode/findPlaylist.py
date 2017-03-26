a14db94c70a1c7b01d5f4f181c08cd56367ff714
Cary McEwan <cary.mcewan@gmail.com>
2017-03-25 22:08:49 -0400

    Added all of the AWS Lambda code.

---
+++ lamdaCode/findPlaylist.py
@@ -0,0 +1,19 @@
+import json
+
+# global playlists
+#
+# with open('playlists.json') as data:
+#     playlists = json.load(data)
+
+def findPlaylist(score):
+    with open('playlists.json') as data:
+        playlists = json.load(data)
+    for playlist in playlists:
+        playlist["dif"] = abs(score - playlist["emotion"])
+
+    playlists = sorted(playlists, key=lambda x : x["dif"], reverse=False)
+    return playlists
+    # for playlist in playlists:
+    #     print(playlist)
+
+# print findPlaylist(-0.1349234)
